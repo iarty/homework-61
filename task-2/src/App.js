21203b2dc1a5772330f3159a10528944a608d143
Barbonov Artem <abarbonov@gmail.com>
2019-12-13 14:22:42 +0600

    add completed task-2

--- task-2/src/App.js
+++ task-2/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import CatBreeds from "./Containers/CatBreeds/CatBreeds";
+import CatSearchImg from "./Containers/CatSearchImg/CatSearchImg";
+import Navigation from "./Components/Navigation/Navigation";
+import CatBase from "./Containers/CatBase/CatBase";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<div>
+			<Navigation />
+			<Switch>
+				<Route path='/breeds' component={CatBreeds}></Route>
+				<Route path='/imgsearch' component={CatSearchImg}></Route>
+				<Route path='/' component={CatBase}></Route>
+			</Switch>
+		</div>
+	);
 }
-
-export default App;
